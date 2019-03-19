ae17a49b1aa5c79ea9d978cb5f9879e0072edd55
senthilsivaraman <senthilkumar150393@gmail.com>
2019-03-19 15:49:10 -0500

    First Changes

---
+++ IntroLearning/IntroLearning/ElseIf.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroLearning
+{
+    class ElseIf
+    {
+        ElseIf()
+        {
+            Console.WriteLine("Control Statement ElseIf block");
+        }
+
+        public static void elseIfMethod()
+        {
+            int a = 100, b = 97, c = 120, d = -200;
+
+            if (a > b && a > c && a > d)
+            {
+                Console.WriteLine("A {0} is greater", a);
+            }
+
+            else if (b > a && b > c && b > d)
+            {
+                Console.WriteLine("B {0} is greater", b);
+            }
+
+            else if (c > a && c > b && c > d)
+            {
+                Console.WriteLine("C {0} is greater", c);
+            }
+
+            else
+            {
+                Console.WriteLine("D {0} is greater", d);
+            }
+        }
+    }
+
+}
---
+++ IntroLearning/IntroLearning/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroLearning
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int option;
+            Console.WriteLine("Available Functions in this Program \n");
+            do
+            {
+                Console.WriteLine(" \n \n 1. Var Data Type \n 2. Type conversion \n 3. Dynamic Data Type \n 4. Else if Control \n 5. Exit");
+                Console.WriteLine("\n Enter your option");
+                option = Convert.ToInt32(Console.ReadLine());
+
+                if (option > 0 && option < 6)
+                {
+                    switch (option)
+                    {
+                        case 1:
+                            VarDataType.varDataBlock();
+                            break;
+                        case 2:
+                            TypeConversion.typeConversionBlock();
+                            break;
+                        case 3:
+                            DynamicDataType.dynamicDataBlock();
+                            break;
+                        case 4:
+                            ElseIf.elseIfMethod();
+                            break;
+                        case 5:
+                            Console.WriteLine("See you");
+                            System.Threading.Thread.Sleep(2000);
+                            Environment.Exit(0);
+                            break;
+
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Exiting..Sorry Try Again Later...");
+                    System.Threading.Thread.Sleep(3000);
+                    Environment.Exit(0);
+
+                }
+
+            } while (option >= 1 && option <= 4);
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+
+}
